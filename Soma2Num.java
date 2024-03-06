88aa0c0ce0a4e57ae9981a28e042cdb131ea49ed
Arthurab352 <138415353+Arthurab352@users.noreply.github.com>
2024-03-06 20:07:05 -0300

    Atividade 1 do Curso
    Soma de dois numeros usando Java

---
+++ Soma2Num.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Soma2Num {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String args[]) {
+		//declaracao de variaveis
+		int num1, num2, soma;
+		
+		//leitura
+		System.out.println("Digite seu número: ");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro numero: ");
+		num2 = sc.nextInt();
+		
+		//Somar
+		soma = num1 + num2;
+		
+		//Mostrar
+		System.out.println("Soma" + soma);
+	}
+}
